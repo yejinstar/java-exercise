62631a9bf6f53cfd72465c138830d1fa49bdcce0
yejinstar <gyj5586@gmail.com>
2022-10-04 15:06:12 +0900

    first commit

---
+++ src/example/ArrayTest.java
@@ -0,0 +1,14 @@
+package example;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
---
+++ src/example/CurrencyTest.java
@@ -0,0 +1,8 @@
+package example;
+
+public class CurrencyTest {
+    public static void main(String[] args) {
+
+
+    }
+}
