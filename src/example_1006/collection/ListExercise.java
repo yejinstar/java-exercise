5b2effe9ed8903133c10f56a06b05373dbd4c04c
yejinstar <gyj5586@gmail.com>
2022-10-06 09:34:03 +0900

    List Exercise

---
+++ src/example_1006/collection/ListExercise.java
@@ -0,0 +1,20 @@
+package example_1006.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExercise {
+    List<String> students;
+
+    public ListExercise() {
+        this.students = new ArrayList<>();
+        students.add("김예진");
+        students.add("김경록");
+        students.add("김미지");
+        students.add("김민지");
+        students.add("김민경");
+    }
+    public List<String> getStudents(){
+        return this.students;
+    }
+}
---
+++ src/example_1006/collection/ListExerciseMain.java
@@ -0,0 +1,15 @@
+package example_1006.collection;
+
+import java.util.List;
+
+public class ListExerciseMain {
+    public static void main(String[] args) {
+        ListExercise listExercise = new ListExercise();
+
+        List<String>students = listExercise.getStudents();
+        for(String student : listExercise.getStudents()){
+            System.out.println(student);
+        }
+        System.out.println(students.size());
+    }
+}
