e494578b931d4b01a1642a4510bff30c89cc9546
yejinstar <gyj5586@gmail.com>
2022-10-06 14:07:46 +0900

    Address Alphabet count

--- src/example_1006/AlphabetCount.java
+++ src/example_1006/AlphabetCount.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 
 public class AlphabetCount {
 
-    boolean isAlphabet(char input) {
-        if (input <= 90 && input >= 65 || input >= 97 && input <= 122) {
+    public boolean isAlphabet(char input) {
+        if (input <= 'Z' && input >= 'A' || input >= 'a' && input <= 'z') {
             return true;
         } else {
             return false;
@@ -14,20 +14,25 @@ public class AlphabetCount {
 
     public static void main(String[] args) {
         // Map => key, value
-
         //String repoAddr = "https://github.com/yejinstar/git_exercise_push";
-        //HashMap<String, String> alphabetCount = new HashMap<>();
+        HashMap<String, Integer> alphabetCountMap = new HashMap<>();
 
-        String repoAddr = "aaabbbcc123!!dddeeffghijkkllll";
+        String repoAddr = "aaabbbcc123!!dddeeffghijkkllll".toUpperCase();
 
         AlphabetCount alphabetCount = new AlphabetCount();
 
-        for (int i = 0; i < repoAddr.length(); i++) {
-
-            System.out.println(alphabetCount.isAlphabet(repoAddr.charAt(i)));
-
+        for (char c = 'A'; c <= 'Z'; c++) {
+            alphabetCountMap.put(String.valueOf(c), 0);
         }
 
+        for (int i = 0; i < repoAddr.length(); i++) {
 
+            char c = repoAddr.charAt(i);
+
+            if (alphabetCount.isAlphabet(c)) {
+                alphabetCountMap.put(String.valueOf(c), alphabetCountMap.get(String.valueOf(c)) + 1);
+            }
+        }
+        System.out.println(alphabetCountMap);
     }
 }
