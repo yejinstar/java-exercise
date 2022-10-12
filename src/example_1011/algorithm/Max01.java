b212f26c8763563bff0a524b84b2e520092d3f8c
yejinstar <gyj5586@gmail.com>
2022-10-12 09:30:53 +0900

    최댓값 찾기 알고리즘

---
+++ src/example_1011/algorithm/Max01.java
@@ -0,0 +1,17 @@
+package example_1011.algorithm;
+
+public class Max01 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 29, 38, 12, 57, 74, 40, 85, 61};
+        int max = 0;
+        int idx = 0;
+        for (int i = 0 ; i < arr.length;i++) {
+            if (max < arr[i]){
+                max = arr[i];
+                idx = i;
+            }
+        }
+        System.out.println("max : " + max);
+        System.out.println("idx : " + idx);
+    }
+}
