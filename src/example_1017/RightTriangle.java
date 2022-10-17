5a4ba71662da938a0a1b662bbb9208d262af76d3
yejinstar <gyj5586@gmail.com>
2022-10-17 09:17:37 +0900

    직각삼각형 찍기 (이중 for)

--- src/example_1017/RightTriangle.java
+++ src/example_1017/RightTriangle.java
@@ -2,9 +2,12 @@ package example_1017;
 
 public class RightTriangle {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("* *");
-        System.out.println("* * *");
-        System.out.println("* * * *");
+
+        for(int i =0 ; i < 4; i ++){
+            for(int j = 0; j <i+1;j++ ){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
