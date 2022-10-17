25edfe6c644608fc51fc5e5295e5b84d8afd69e1
yejinstar <gyj5586@gmail.com>
2022-10-17 09:07:54 +0900

    직각삼각형 찍기

---
+++ src/example_1017/RightTriangle.java
@@ -0,0 +1,10 @@
+package example_1017;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
